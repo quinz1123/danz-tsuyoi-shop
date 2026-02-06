ce4b708bd905759c9e3c859f0da3bb5651ff90fc
quinz1123 <quinzzz111@gmail.com>
2026-02-06 17:48:35 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,10 +2,10 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebas
 import { getAuth, signInWithEmailAndPassword,onAuthStateChanged,signOut } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js"
 
 const firebaseConfig = {
- apiKey: "ISI_API_KEY",
- authDomain: "ISI_AUTH_DOMAIN",
- projectId: "ISI_PROJECT_ID",
- appId: "ISI_APP_ID"
+ apiKey: "AIzaSyA2Eb7HpVNE7yPKsYxNqdCNs78qCkov62U",
+ authDomain: "danz-tsuyoi.firebaseapp.com",
+ projectId: "danz-tsuyoi",
+ appId: "1:504620812619:web:02d66470fa3bed9fbfc0ce"
 }
 
 const app = initializeApp(firebaseConfig)
