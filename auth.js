bcae847747be5369b8d91117431e40b7e9cadfba
quinz1123 <quinzzz111@gmail.com>
2026-02-06 13:21:39 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,15 +1,38 @@
-import {auth} from "/firebase.js"
-import {
-signInWithEmailAndPassword,
-createUserWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"
 
-window.login=async()=>{
-await signInWithEmailAndPassword(auth,email.value,password.value)
-location.href="/index.html"
-}
+import { getAuth,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-window.register=async()=>{
-await createUserWithEmailAndPassword(auth,email.value,password.value)
-location.href="/index.html"
-}
+import { app } from "./firebase.js";
+
+const auth = getAuth(app);
+
+window.register = () => {
+
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+if(!email || !password) return alert("Lengkapi data");
+
+createUserWithEmailAndPassword(auth,email,password)
+.then(()=>{
+alert("Register berhasil");
+location.href="/login.html";
+})
+.catch(e=>alert(e.message));
+
+};
+
+window.login = () => {
+
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+signInWithEmailAndPassword(auth,email,password)
+.then(()=>{
+location.href="/dashboard.html";
+})
+.catch(()=>alert("Login gagal"));
+
+};
