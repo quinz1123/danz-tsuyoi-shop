76c4095c13e71452440d8418b4ea049a9c0fd664
quinz1123 <quinzzz111@gmail.com>
2026-02-06 18:06:04 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,9 +1,11 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js"
 import {
 getAuth,
 signInWithEmailAndPassword,
 createUserWithEmailAndPassword,
-onAuthStateChanged
+onAuthStateChanged,
+signOut
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js"
 
 const firebaseConfig = {
@@ -16,11 +18,19 @@ appId: "1:504620812619:web:02d66470fa3bed9fbfc0ce"
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 
-// AUTO REDIRECT
+// AUTO GUARD
 onAuthStateChanged(auth,user=>{
-if(user && location.pathname.includes("login")) location.href="/"
-if(!user && !location.pathname.includes("login") && !location.pathname.includes("register"))
+const path = location.pathname
+
+if(user){
+if(path.includes("login") || path.includes("register")){
+location.href="/"
+}
+}else{
+if(!path.includes("login") && !path.includes("register")){
 location.href="/login.html"
+}
+}
 })
 
 // LOGIN
@@ -34,7 +44,8 @@ signInWithEmailAndPassword(auth,email.value,password.value)
 window.register=()=>{
 createUserWithEmailAndPassword(auth,email.value,password.value)
 .then(()=>{
-alert("Daftar berhasil")
+alert("Daftar berhasil, silakan login")
+signOut(auth)
 location.href="/login.html"
 })
 .catch(e=>alert(e.message))
