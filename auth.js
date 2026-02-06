f0f2f54644b20cb744199798cd72eb7e28000752
quinz1123 <quinzzz111@gmail.com>
2026-02-06 12:30:26 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
-import {auth} from "./firebase.js"
+import {auth} from "/firebase.js"
 import {signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"
 
 window.login=async()=>{
 await signInWithEmailAndPassword(auth,email.value,password.value)
-location.href="index.html"
+location.href="/index.html"
 }
