5b76911ee03ea846afba3091977c7caf3390513f
quinz1123 <quinzzz111@gmail.com>
2026-02-06 18:18:35 +0700

    Update guard.js

--- guard.js
+++ guard.js
@@ -1,17 +1,5 @@
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js"
-
-const firebaseConfig = {
-apiKey: "AIzaSyA2Eb7HpVNE7yPKsYxNqdCNs78qCkov62U",
-authDomain: "danz-tsuyoi.firebaseapp.com",
-projectId: "danz-tsuyoi",
-appId: "1:504620812619:web:02d66470fa3bed9fbfc0ce"
-}
-
-initializeApp(firebaseConfig)
-
 setTimeout(()=>{
-if(!localStorage.getItem("logged")){
+if(!sessionStorage.getItem("login")){
 location.replace("/login.html")
 }
-},200)
+},300)
