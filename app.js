1b0e1819f45f60474654899847a0dd063abeb5df
quinz1123 <quinzzz111@gmail.com>
2026-02-06 20:02:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,66 +1,57 @@
-
 const btn = document.getElementById("btn")
 const out = document.getElementById("out")
 const token = document.getElementById("token")
 const username = document.getElementById("username")
 const ram = document.getElementById("ram")
 
-let isProcessing = false
-let lastUsername = null
+let isProcessing=false
+let lastUsername=null
 
-btn.onclick = async () => {
+btn.onclick = async ()=>{
 
-const currentUsername = username.value.trim()
-const currentToken = token.value.trim()
+const u=username.value.trim()
+const t=token.value.trim()
 
-if(!currentToken){
-alert("Token kosong")
-return
-}
+if(!t) return alert("Token kosong")
+if(!u) return alert("Username kosong")
 
-if(!currentUsername){
-alert("Username kosong")
-return
-}
-
-// BLOCK SAME USERNAME
-if(lastUsername === currentUsername){
-alert("Silakan ganti username baru terlebih dahulu.")
+if(lastUsername===u){
+alert("Ganti username baru dulu")
 return
 }
 
 if(isProcessing) return
-isProcessing = true
+isProcessing=true
 
-btn.disabled = true
+btn.disabled=true
 btn.innerText="PROCESS..."
 
-out.innerHTML = `<div class="loader"></div>`
+out.innerHTML=`<div class="loader"></div>`
 
 try{
 
-const r = await fetch("/api/create",{
+const r=await fetch("/api/create",{
 method:"POST",
 headers:{'Content-Type':'application/json'},
 body:JSON.stringify({
-username:currentUsername,
-token:currentToken,
+username:u,
+token:t,
 ram:ram.value
 })
 })
 
-const j = await r.json()
+const j=await r.json()
 
 if(j.error){
-out.innerHTML = `<div style="color:red">${j.error}</div>`
+out.innerHTML=`<div style="color:red">${j.error}</div>`
 reset()
 return
 }
 
-// SAVE LAST USERNAME
-lastUsername = currentUsername
+lastUsername=u
 
-out.innerHTML = `
+out.innerHTML=`
+
 <div class="result-card">
 
 <h3>✅ PANEL BERHASIL DIBUAT</h3>
@@ -71,15 +62,15 @@ out.innerHTML = `
 <p><b>Server ID:</b> ${j.server_id}</p>
 <p><b>RAM:</b> ${j.ram}</p>
 
-<button onclick="window.open('${j.panel}','_blank')" class="action">
+<button class="action" onclick="window.open('${j.panel}','_blank')">
 BUKA PANEL
 </button>
 
-<button onclick="navigator.clipboard.writeText('${j.password}')" class="action dark">
+<button class="action dark" onclick="navigator.clipboard.writeText('${j.password}')">
 COPY PASSWORD
 </button>
 
-<button onclick="navigator.clipboard.writeText('${j.username}')" class="action dark">
+<button class="action dark" onclick="navigator.clipboard.writeText('${j.username}')">
 COPY USERNAME
 </button>
 
@@ -87,9 +78,7 @@ COPY USERNAME
 `
 
 }catch(e){
-
 out.innerHTML="<div style='color:red'>ERROR CONNECTION</div>"
-
 }
 
 reset()
