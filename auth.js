31d197a6f2fd0d6fa52cfcac7e393d4860be1f1e
quinz1123 <quinzzz111@gmail.com>
2026-02-06 18:12:42 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,9 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebas
 import {
 getAuth,
 signInWithEmailAndPassword,
-createUserWithEmailAndPassword,
-onAuthStateChanged,
-signOut
+createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js"
 
 const firebaseConfig = {
@@ -17,30 +15,13 @@ appId: "1:504620812619:web:02d66470fa3bed9fbfc0ce"
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 
-// CEGAH LOOP
-let checked = false
-
-onAuthStateChanged(auth,user=>{
-if(checked) return
-checked = true
-
-const path = location.pathname
-
-if(user){
-if(path.includes("login") || path.includes("register")){
-location.replace("/")
-}
-}else{
-if(!path.includes("login") && !path.includes("register")){
-location.replace("/login.html")
-}
-}
-})
-
 // LOGIN
 window.login=()=>{
 signInWithEmailAndPassword(auth,email.value,password.value)
-.then(()=>location.replace("/"))
+.then(()=>{
+localStorage.setItem("logged","yes")
+location.href="/"
+})
 .catch(e=>alert(e.message))
 }
 
@@ -49,8 +30,7 @@ window.register=()=>{
 createUserWithEmailAndPassword(auth,email.value,password.value)
 .then(()=>{
 alert("Daftar berhasil")
-signOut(auth)
-location.replace("/login.html")
+location.href="/login.html"
 })
 .catch(e=>alert(e.message))
 }
