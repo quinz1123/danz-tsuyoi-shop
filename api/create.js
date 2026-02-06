70d92d2934bee9c96acfa13229717c9d25abd8eb
quinz1123 <quinzzz111@gmail.com>
2026-02-06 16:29:54 +0700

    Update create.js

--- api/create.js
+++ api/create.js
@@ -11,12 +11,13 @@ return res.status(405).json({error:"Method not allowed"})
 const {username,token,ram}=req.body
 
 if(!username) return res.json({error:"Username kosong"})
-if(token!=="RIZKY123") return res.json({error:"Token salah"})
+if(token!=="Danz123") return res.json({error:"Token salah"})
 
 const PANEL="https://danz-tsuyoi.flixiazone.my.id"
-const PTLA="ptla_HDoMsQBrkjxBYtS0ei6h4pV7NwBHPxZfqgmQBMeczbv"   // <<< WAJIB GANTI
+const PTLA="ptla_HDoMsQBrkjxBYtS0ei6h4pV7NwBHPxZfqgmQBMeczbv"
 
-// CREATE USER
+// ================= CREATE USER =================
+
 const userReq = await fetch(PANEL+"/api/application/users",{
 method:"POST",
 headers:{
@@ -36,12 +37,15 @@ password:username+"001"
 const user = await userReq.json()
 
 if(!user.attributes){
-return res.json({error:user.errors?.[0]?.detail || "Gagal create user"})
+return res.json({
+error:user.errors?.[0]?.detail || "Gagal create user"
+})
 }
 
 const uid = user.attributes.id
 
-// CREATE SERVER
+// ================= CREATE SERVER =================
+
 const serverReq = await fetch(PANEL+"/api/application/servers",{
 method:"POST",
 headers:{
@@ -50,12 +54,17 @@ headers:{
 "Accept":"application/json"
 },
 body:JSON.stringify({
+
 name:username,
 user:uid,
 egg:15,
 docker_image:"ghcr.io/pterodactyl/yolks:debian",
 startup:"bash",
-environment:{},
+
+environment:{
+CMD_RUN:"node index.js"   // FIX ERROR COMMAND RUN
+},
+
 limits:{
 memory:Number(ram),
 swap:0,
@@ -63,23 +72,28 @@ disk:0,
 io:500,
 cpu:0
 },
+
 feature_limits:{
 databases:5,
 backups:5,
 allocations:1
 },
+
 deploy:{
 locations:[1],
 dedicated_ip:false,
 port_range:[]
 }
+
 })
 })
 
 const server = await serverReq.json()
 
 if(!server.attributes){
-return res.json({error:server.errors?.[0]?.detail || "Gagal create server"})
+return res.json({
+error:server.errors?.[0]?.detail || "Gagal create server"
+})
 }
 
 return res.json({
