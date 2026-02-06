a58b86cde5ce34da66864d228308cf140434bb2f
quinz1123 <quinzzz111@gmail.com>
2026-02-06 18:31:00 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,62 +2,50 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js"
 import {
 getAuth,
+onAuthStateChanged,
 signInWithEmailAndPassword,
 createUserWithEmailAndPassword,
-onAuthStateChanged,
 signOut
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js"
 
 const firebaseConfig = {
-apiKey: "ISI_APIKEY_KAMU",
-authDomain: "ISI_AUTHDOMAIN",
-projectId: "ISI_PROJECTID",
-appId: "ISI_APPID"
+apiKey: "AIzaSyA2Eb7HpVNE7yPKsYxNqdCNs78qCkov62U",
+authDomain: "danz-tsuyoi.firebaseapp.com",
+projectId: "danz-tsuyoi",
+appId: "1:504620812619:web:02d66470fa3bed9fbfc0ce"
 }
 
-const app = initializeApp(firebaseConfig)
-const auth = getAuth(app)
+initializeApp(firebaseConfig)
+const auth = getAuth()
 
-/* LOGIN */
-window.login = function(){
-let email = document.getElementById("email").value
-let pass = document.getElementById("password").value
-
-signInWithEmailAndPassword(auth,email,pass)
-.then(()=>{
-sessionStorage.setItem("login","true")
-location.replace("/")
-})
-.catch(e=>alert(e.message))
-}
-
-/* REGISTER */
-window.register = function(){
-let email = document.getElementById("email").value
-let pass = document.getElementById("password").value
-
-createUserWithEmailAndPassword(auth,email,pass)
-.then(()=>{
-alert("Register sukses")
-location.href="login.html"
-})
-.catch(e=>alert(e.message))
-}
-
-/* LOGOUT */
-window.logout = function(){
-signOut(auth)
-.then(()=>{
-sessionStorage.clear()
-location.replace("login.html")
-})
-}
-
-/* GUARD */
 onAuthStateChanged(auth,user=>{
 if(user){
-sessionStorage.setItem("login","true")
+sessionStorage.setItem("login","1")
 }else{
 sessionStorage.removeItem("login")
 }
 })
+
+window.login=(e,p)=>{
+signInWithEmailAndPassword(auth,e,p)
+.then(()=>{
+sessionStorage.setItem("login","1")
+location.replace("/")
+})
+.catch(x=>alert(x.message))
+}
+
+window.register=(e,p)=>{
+createUserWithEmailAndPassword(auth,e,p)
+.then(()=>{
+alert("Daftar berhasil")
+})
+.catch(x=>alert(x.message))
+}
+
+window.logout=()=>{
+signOut(auth).then(()=>{
+sessionStorage.clear()
+location.replace("/login.html")
+})
+}
