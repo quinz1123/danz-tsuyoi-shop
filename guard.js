d4677f2f1c5968ab743e05634ea97738ae8aa87c
quinz1123 <quinzzz111@gmail.com>
2026-02-07 09:09:51 +0700

    Update guard.js

--- guard.js
+++ guard.js
@@ -1,5 +1,17 @@
-await new Promise(r=>setTimeout(r,300))
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js"
+import { getAuth,onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js"
 
-if(!localStorage.getItem("logged")){
+const firebaseConfig={
+apiKey:"AIzaSyA2Eb7HpVNE7yPKsYxNqdCNs78qCkov62U",
+authDomain:"danz-tsuyoi.firebaseapp.com",
+projectId:"danz-tsuyoi"
+}
+
+const app=initializeApp(firebaseConfig)
+const auth=getAuth(app)
+
+onAuthStateChanged(auth,user=>{
+if(!user){
 location.replace("login.html")
 }
+})
