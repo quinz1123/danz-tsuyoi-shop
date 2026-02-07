6b584ffa452848fc1d12a638ce2f2877a2036f63
quinz1123 <quinzzz111@gmail.com>
2026-02-07 09:19:09 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js"
 import {
 getAuth,
@@ -20,28 +19,25 @@ projectId:"danz-tsuyoi"
 const app=initializeApp(firebaseConfig)
 const auth=getAuth(app)
 
-// SATU-SATUNYA AUTH GUARD
+// GLOBAL AUTH LISTENER
 onAuthStateChanged(auth,user=>{
 
 const path=location.pathname
 
 if(user){
 
-// email login wajib verified
 if(user.providerData[0]?.providerId!=="google.com" && !user.emailVerified){
 alert("Verifikasi email dulu")
 signOut(auth)
 return
 }
 
-// kalau sudah login tapi masih di login page
 if(path.includes("login")||path.includes("register")){
 location.replace("/")
 }
 
 }else{
 
-// belum login tapi buka halaman dalam
 if(!path.includes("login")&&!path.includes("register")){
 location.replace("login.html")
 }
@@ -50,33 +46,24 @@ location.replace("login.html")
 
 })
 
-// LOGIN EMAIL
+// LOGIN
 window.login=function(){
-
-signInWithEmailAndPassword(
-auth,
-email.value.trim(),
-password.value.trim()
-).catch(e=>alert(e.message))
-
+signInWithEmailAndPassword(auth,email.value,password.value)
+.catch(e=>alert(e.message))
 }
 
 // REGISTER
 window.register=function(){
-
-createUserWithEmailAndPassword(
-auth,
-email.value.trim(),
-password.value.trim()
-).then(r=>{
+createUserWithEmailAndPassword(auth,email.value,password.value)
+.then(r=>{
 sendEmailVerification(r.user)
-alert("OTP dikirim ke email")
+alert("OTP dikirim")
 location.replace("login.html")
-}).catch(e=>alert(e.message))
-
+})
+.catch(e=>alert(e.message))
 }
 
-// GOOGLE LOGIN
+// GOOGLE
 window.googleLogin=function(){
 signInWithPopup(auth,new GoogleAuthProvider())
 .catch(e=>alert(e.message))
